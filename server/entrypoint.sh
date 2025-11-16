17ea77939fdbb4d5279d0832f3495f8952879f32
Luka Klacar <lukaklacar@gmail.com>
2025-11-16 01:00:00 +0100

    Enhance entrypoint and WebSocket handling with RCON support and auto-kick functionality

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -5,8 +5,10 @@ set -Eeuo pipefail
 node ./ws-udp-proxy/index.js &
 
 ./build/Release/ioq3ded \
-                 +set dedicated 2 \
-                 +set sv_pure 1 \
-                 +set net_ip 0.0.0.0 \
-                 +set sv_master3 "192.168.0.11" \
-                 +map q3dm17
+  +set dedicated 2 \
+  +set sv_pure 1 \
+  +set net_ip 0.0.0.0 \
+  +set sv_master3 "192.168.0.11" \
+  +set sv_maxclients 64 \
+  +set rconPassword "qweqwe1*" \
+  +map q3dm17
