56ff9acf75d8d16b746292d3bef4a42b34576c73
Luka Klacar <lukaklacar@gmail.com>
2025-11-16 01:00:00 +0100

    Enhance bot configuration in entrypoint script for improved gameplay dynamics

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -11,7 +11,12 @@ node ./ws-udp-proxy/index.js &
   +set sv_maxclients 64 \
   +set rconPassword "${RCON_PASSWORD}" \
   +set sv_hostname "Q3DM17 24/7" \
+  +set g_inactivity 0 \
+  +set bot_enable 1 \
   +set bot_minplayers 2 \
-  +addbot sarge 2 \
-  +addbot daemia 2 \
+  +set timelimit 0 \
+  +set fraglimit 0 \
+  +set sv_maxPing 500 \
+  +set sv_minPing 0 \
   +map q3dm17
+
