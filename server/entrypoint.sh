160df0b97fa3b599a6600e13b169bb9493048837
Luka Klacar <lukaklacar@gmail.com>
2025-11-16 01:00:00 +0100

    Refactor entrypoint script to remove unnecessary server settings and streamline initialization

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -8,16 +8,5 @@ node ./ws-udp-proxy/index.js &
   +set sv_pure 1 \
   +set net_ip 0.0.0.0 \
   +set sv_master3 "master.q3js.com:27950" \
-  +set sv_maxclients 64 \
-  +set rconPassword "${RCON_PASSWORD}" \
-  +set sv_hostname "FFA 24/7" \
-  +set g_inactivity 360 \
-  +set g_dropInactive 1 \
-  +set bot_enable 1 \
-  +set timelimit 0 \
-  +set fraglimit 30 \
-  +set sv_maxPing 500 \
-  +set sv_minPing 0 \
-  +set sv_dlRate 0 \
   +map q3dm17 \
   +exec autoexec.cfg
