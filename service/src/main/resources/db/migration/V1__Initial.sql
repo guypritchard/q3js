028b638ee1d7eeae61c5da047ec0de84ee37c837
Luka Klacar <lukaklacar@gmail.com>
2025-11-16 01:00:00 +0100

    Removed service

--- service/src/main/resources/db/migration/V1__Initial.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE server
-(
-    id         SERIAL PRIMARY KEY,
-    name       VARCHAR(255) NOT NULL,
-    port       INT          NOT NULL,
-    status     VARCHAR(50)  NOT NULL,
-    ip_address VARCHAR(45)  NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
