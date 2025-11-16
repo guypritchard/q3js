dd61f82fb48b2be717cd0d60bc4bb80e08d97a52
Luka Klacar <lukaklacar@gmail.com>
2025-11-16 01:00:00 +0100

    Update entrypoint.sh to set server hostname for Q3DM17

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -Eeuo pipefail
 
 node ./ws-udp-proxy/index.js &
@@ -11,4 +10,5 @@ node ./ws-udp-proxy/index.js &
   +set sv_master3 "192.168.0.11" \
   +set sv_maxclients 64 \
   +set rconPassword "qweqwe1*" \
+  +set sv_hostname "Q3DM17 24/7" \
   +map q3dm17
