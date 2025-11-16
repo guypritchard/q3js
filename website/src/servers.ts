9e82c14b8cab37eaababd2072c534cbca71707d1
Luka Klacar <lukaklacar@gmail.com>
2025-11-16 01:00:00 +0100

    Add CTF server configuration to SERVER_LIST

--- website/src/servers.ts
+++ website/src/servers.ts
@@ -2,11 +2,20 @@ import {env} from "@/env.ts";
 import {getWsProtocol} from "@/lib/utils.ts";
 
 const SERVER_LIST = [
+    // FFA
     {
         location: "EU",
         proxy: `${getWsProtocol()}//${env.VITE_PROXY_URL}`,
         host: "88.99.66.204",
         port: 27960,
+    },
+
+    // CTF
+    {
+        location: "EU",
+        proxy: `${getWsProtocol()}//${env.VITE_PROXY_URL}`,
+        host: "88.99.66.204",
+        port: 37960,
     },
 ]
 
