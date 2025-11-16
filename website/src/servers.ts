ad69b78d6400afcef792df64401da55cbfd1b279
Luka Klacar <lukaklacar@gmail.com>
2025-11-16 01:00:00 +0100

    Update server configuration with new host and port for EU location

--- website/src/servers.ts
+++ website/src/servers.ts
@@ -6,8 +6,8 @@ const SERVER_LIST = [
     {
         location: "EU",
         proxy: `${getWsProtocol()}//${env.VITE_PROXY_URL}`,
-        host: "88.99.66.204",
-        port: 27960,
+        host: "ffa.q3js.com",
+        port: 80,
     },
 ]
 
